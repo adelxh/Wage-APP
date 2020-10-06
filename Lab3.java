1385b26666b57c6010da77beba9db4c0d983c644
adelxh <adelxh@gmail.com>
2020-10-06 18:13:19 -0400

    added all methods

--- Lab3.java
+++ Lab3.java
@@ -36,7 +36,8 @@ do {
                 break;
                 
             case 4: 
-                BasePlusCommissionEmployee(); 
+                // BasePlusCommissionEmployee(); 
+                System.out.println("not the right option"); 
                 break; 
 
             default:
@@ -49,11 +50,40 @@ do {
 
 
    public static void CommissionEmployee() {
+       Scanner input = new Scanner(System.in); 
        double grossSales; 
-       double commissionRate;
+       double commissionRate = 6.75;
+
+       System.out.println("Enter gross Sales: ");
+       grossSales = input.nextDouble();
+
+    double result = grossSales * commissionRate; 
+    System.out.println("your salary is: " + result);
 
    }
 
+   public static void HourlyEmployee() {
+       Scanner input = new Scanner(System.in);
+       double wage; 
+       double hours; 
+       System.out.println("Please enter your wage: ");
+       wage = input.nextDouble();
+       System.out.println("Please enter how many hours you have worked: ");
+       hours = input.nextDouble(); 
+       double resultWage = wage * hours; 
+       System.out.println("your salary for " + hours + " hours is $" + resultWage);
+
+   }
+
+   public static void SalariedEmployee() {
+       Scanner input = new Scanner(System.in); 
+       double weeklySalary; 
+
+       System.out.println("please enter your weekly salary: ");
+       weeklySalary = input.nextDouble(); 
+       System.out.println("your weekly salary is: $" + weeklySalary);
+
+   }
 
 }
 
