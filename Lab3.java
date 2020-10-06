b6a4797aa010aca736fcbba5ffc5deac7fa71579
adelxh <adelxh@gmail.com>
2020-10-06 17:36:43 -0400

    first commit, added commision employee method

--- Lab3.java
+++ Lab3.java
@@ -10,6 +10,7 @@ public class Lab3 {
 
     public static void Employee() {
         Scanner input = new Scanner(System.in);
+        boolean tr = true;
     System.out.println();
         System.out.println("Wage APP"); 
         System.out.println("-------------");
@@ -19,6 +20,40 @@ public class Lab3 {
         System.out.println("Hourly employee");
         System.out.println("Base plus Commision employee");
         int chooseStatus = input.nextInt();
-    }
+do {
+        switch(chooseStatus) {
+
+            case 1: 
+                CommissionEmployee(); 
+                break; 
+            
+            case 2: 
+                SalariedEmployee(); 
+                break;
+                
+            case 3:
+                HourlyEmployee();
+                break;
+                
+            case 4: 
+                BasePlusCommissionEmployee(); 
+                break; 
+
+            default:
+            tr = false;
+            break;
+        }
+    } while(tr == true);
+    
+   }
+
+
+   public static void CommissionEmployee() {
+       double grossSales; 
+       double commissionRate;
+
+   }
+
+
 }
 
