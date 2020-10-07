f1a02525afdb9e7ed17dba202cceb2d0883e1bf0
Adel Akhmed <adelxh@gmail.com>
2020-10-07 00:26:45 -0400

    added interface

--- Lab3.java
+++ Lab3.java
@@ -1,89 +1,94 @@
-import java.util.Scanner; 
+public interface Payable {
 
-public class Lab3 {
+    double getPaymentAmount(); // get payment amount method only declared 
 
-    public static void main(String[] args) {
+    public class Invoice implements Payable {
 
-        Employee();
+        private String partNum; 
+        private String Partdescription; 
+        private int partCount; 
+        private double partPrice; 
+
+        public Invoice(String part, String description, int count, double price) {
+
+            partNum = part; 
+            Partdescription = description; 
+            partCount = count; 
+            partPrice = price; 
+        }
+
+        // getter function for the num
+        public String getPartNum(String part) {
+            return part; 
+            
+        }
+
+        // setter function for the num
+        public void setPartNum(String part) {
+           partNum = part; 
+        }
+
+        // getter function for description 
+        public String getPartDescription(String description) {
+            return description; 
+        }
+
+        // setter function for description 
+        public void setPartDescription(String description) {
+            Partdescription = description; 
+        }
+
+        public int getPartCount() {
+            return partCount;
+           
+        }
+
+        public void setPartCount(int count) {
+            if (count >= 1) {
+                partCount = count; 
+            } else {
+                throw new IllegalArgumentException("has to be above 0");
+            }
+
+        }
+
+        // getter function for price 
+        public Double getPartPrice() {
+                return partPrice; 
+        }
+
+        // setter function for price 
+        public void setPartPrice(double price) {
+
+            if (partPrice > 0.0) {
+                partPrice = price;
+            }
+            else {
+                throw new IllegalArgumentException("enter price above 0.0");
+            }
+        }
+
+
+        @Override
+        public String toString() {
+            return String.format("%s: \n\s:", "invoice", "part number", getPartNum(partNum), getPartDescription(Partdescription), "quantity", "price per item", getPartCount(), getPartPrice());
+        }
+        
+        @Override
+    public double getPaymentAmount() {
+        return getPartCount() * getPartPrice(); 
 
     }
 
-    public static void Employee() {
-        Scanner input = new Scanner(System.in);
-        boolean tr = true;
-    System.out.println();
-        System.out.println("Wage APP"); 
-        System.out.println("-------------");
-        System.out.println("choose your status: ");
-        System.out.println("Commision employee");
-        System.out.println("Salaried employee");
-        System.out.println("Hourly employee");
-        System.out.println("Base plus Commision employee");
-        int chooseStatus = input.nextInt();
-do {
-        switch(chooseStatus) {
-
-            case 1: 
-                CommissionEmployee(); 
-                break; 
-            
-            case 2: 
-                SalariedEmployee(); 
-                break;
-                
-            case 3:
-                HourlyEmployee();
-                break;
-                
-            case 4: 
-                // BasePlusCommissionEmployee(); 
-                System.out.println("not the right option"); 
-                break; 
-
-            default:
-            tr = false;
-            break;
-        }
-    } while(tr == true);
-    
-   }
 
 
-   public static void CommissionEmployee() {
-       Scanner input = new Scanner(System.in); 
-       double grossSales; 
-       double commissionRate = 6.75;
 
-       System.out.println("Enter gross Sales: ");
-       grossSales = input.nextDouble();
+    }
 
-    double result = grossSales * commissionRate; 
-    System.out.println("your salary is: " + result);
+   
 
-   }
-
-   public static void HourlyEmployee() {
-       Scanner input = new Scanner(System.in);
-       double wage; 
-       double hours; 
-       System.out.println("Please enter your wage: ");
-       wage = input.nextDouble();
-       System.out.println("Please enter how many hours you have worked: ");
-       hours = input.nextDouble(); 
-       double resultWage = wage * hours; 
-       System.out.println("your salary for " + hours + " hours is $" + resultWage);
-
-   }
-
-   public static void SalariedEmployee() {
-       Scanner input = new Scanner(System.in); 
-       double weeklySalary; 
-
-       System.out.println("please enter your weekly salary: ");
-       weeklySalary = input.nextDouble(); 
-       System.out.println("your weekly salary is: $" + weeklySalary);
-
-   }
-
+   
 }
 
+ 
+   
